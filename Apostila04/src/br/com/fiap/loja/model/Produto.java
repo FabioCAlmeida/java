ef98c0bb38670f2ed882516a79fbe4f15d2c5e5d
FabioCAlmeida <fabiocezare869@gmail.com>
2026-03-20 21:55:38 -0300

    atualizando terminal

--- Apostila04/src/br/com/fiap/loja/model/Produto.java
+++ Apostila04/src/br/com/fiap/loja/model/Produto.java
@@ -2,8 +2,9 @@ package br.com.fiap.loja.model;
 
 public class Produto {
 
-    double preco;
-    int quantidade;
-    String nome;
-    boolean garantia;
+    public Categoria categoria;
+    public double preco;
+    public int quantidade;
+    public String nome;
+    public boolean garantia;
 }
--- Apostila04/src/br/com/fiap/loja/view/Terminal.java
+++ Apostila04/src/br/com/fiap/loja/view/Terminal.java
@@ -1,18 +1,61 @@
 package br.com.fiap.loja.view;
 
+import br.com.fiap.loja.model.Categoria;
+import br.com.fiap.loja.model.Produto;
+
+import java.util.Scanner;
+
 public class Terminal {
 
-
     public static void main(String[] args) {
-        //ler os dados do produto 
-        //criar o objeto produto
-        //atribuir os valores dos aributos
 
-        //exibir os valores dos atributos
+        Scanner entrada = new Scanner(System.in);
+
+        // ler os dados do produto
+        System.out.println("Digite o nome do produto:");
+        String nome = entrada.nextLine();
+
+        System.out.println("Digite o preço do produto:");
+        double valor = entrada.nextDouble();
+
+        System.out.println("Digite a quantidade do produto:");
+        int quantidade = entrada.nextInt();
+
+        System.out.println("O produto possui garantia? (true/false)");
+        boolean garantia = entrada.nextBoolean();
+        entrada.nextLine(); // limpar buffer
+
+        // categoria
+        System.out.println("Digite o nome da categoria:");
+        String nomeCategoria = entrada.nextLine();
+
+        System.out.println("Digite a descrição da categoria:");
+        String descricaoCategoria = entrada.nextLine();
+
+        // criar objeto produto
+        Produto produto = new Produto();
+        produto.nome = nome;
+        produto.preco = valor;
+        produto.quantidade = quantidade;
+        produto.garantia = garantia;
+
+
+        Categoria categoria = new Categoria();
+        categoria.nome = nomeCategoria;
+        categoria.descricao = descricaoCategoria;
+
+
+        produto.categoria = categoria;
+
+        // exibir dados
+        System.out.println("\nProduto cadastrado:");
+        System.out.println("Nome: " + produto.nome);
+        System.out.println("Preço: " + produto.preco);
+        System.out.println("Quantidade: " + produto.quantidade);
+        System.out.println("Garantia: " + produto.garantia);
+        System.out.println("Categoria: " + produto.categoria.nome);
+        System.out.println("Descrição: " + produto.categoria.descricao);
+
+        entrada.close();
     }
-
-
-
-
-    }
-
+}
